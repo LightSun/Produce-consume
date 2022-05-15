b20fa422af5cc3b33488f1084c01dc84bfb364da
heaven7 <978136772@qq.com>
2022-05-15 17:17:24 +0800

    opt pair

--- Produce-consume/core/src/main/java/com/heaven7/java/pc/utils/Pair.java
+++ Produce-consume/core/src/main/java/com/heaven7/java/pc/utils/Pair.java
@@ -1,17 +1,37 @@
 package com.heaven7.java.pc.utils;
 
+import java.util.Map;
+
 /**
  * the pair
  * @param <K> the key type
  * @param <V> the value type
  * @since 1.0.5
  */
-public class Pair<K, V> {
-    public final K key;
-    public final V value;
+public class Pair<K, V> implements Map.Entry<K, V>{
+
+    public K key;
+    public V value;
 
     public Pair(K key, V value) {
         this.key = key;
         this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V val) {
+        V old = value;
+        this.value = val;
+        return old;
     }
 }
